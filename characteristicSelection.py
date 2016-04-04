1fe916e73cf1ceab9cf40d8be49b891d71bb7651
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2016-04-05 00:50:51 +0200

    SFS

--- characteristicSelection.py
+++ characteristicSelection.py
@@ -1,33 +1,64 @@
+import time
 import numpy as np
 from scipy.io import arff
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import StratifiedKFold
+from sklearn.preprocessing import MinMaxScaler
+
+from algorithms.greedy import SFS
 
 # Read data
-data, metaData = arff.loadarff("./data/arrhythmia.arff")
+data, metaData = arff.loadarff("./data/wdbc.arff")
 
 # Divide features data and classes classification
-train = data[:][metaData.names()[:-1]]
-target = data[:]["class"]
+train = data[metaData.names()[:-1]]
+target = data["class"]
 
 # Encapsulate all data in a dictionary
 data = {
     # Necessary to deal with numpy.void
-    "train": np.asarray(train.tolist(), dtype=np.float32),
-    "target": target
+    "features": np.asarray(train.tolist(), dtype=np.float32),
+    "target": np.asarray(target.tolist(), dtype=np.int32)
 }
 
-skf = StratifiedKFold(data["target"], 2, shuffle=True)
+# Normalize data
+normalizer = MinMaxScaler()
+data["features"] = normalizer.fit_transform(data["features"])
 
-for idxTrain, idxTest in skf:
-    # Initialize 3-NN classifier
-    knnClassifier = KNeighborsClassifier(n_neighbors=3, n_jobs=8)
+# Initialize 3-NN classifier
+knnClassifier = KNeighborsClassifier(n_neighbors=3, n_jobs=8)
 
-    # Fit the data -run the classifier-
-    knnClassifier.fit(data["train"][idxTrain], data["target"][idxTrain])
+# Define number of experiments
+numExperiments = 5
 
-    # Get the score
-    score = knnClassifier.score(data["train"][idxTest],
-                                data["target"][idxTest])
-                                
-    print(score)
+for i in range(numExperiments):
+    # Make the partitions
+    partitions = StratifiedKFold(data["target"], 2, shuffle=True)
+
+    for idxTrain, idxTest in partitions:
+        # Define training data partitions
+        featuresTrain = data["features"][idxTrain]
+        targetTrain = data["target"][idxTrain]
+
+        # Define test data partitions
+        featuresTest = data["features"][idxTest]
+        targetTest = data["target"][idxTest]
+
+        # Select features and measure time
+        start = time.time()
+        solution = SFS(featuresTrain, targetTrain, knnClassifier)
+        end = time.time()
+
+        # Get the indices of the selected features
+        selectedFeatures = solution.nonzero()[0]
+
+        # Fit the training data -run the classifier-
+        knnClassifier.fit(featuresTrain[:, selectedFeatures], targetTrain)
+
+        # Get the score with the test data
+        score = knnClassifier.score(featuresTest[:, selectedFeatures],
+                                    targetTest)
+
+        print("Tiempo:\t", end-start)
+        print("Score :\t", score)
+        print("*--"*50, "*")
